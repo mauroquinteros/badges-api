11872abbb2a839142025d87f6ac8561a258394ab
mauroquinteros <quinterosmauro0599@gmail.com>
2020-10-21 00:35:47 -0500

    Add utils - V0.3.2

---
+++ src/utils/index.js
@@ -0,0 +1,19 @@
+const md5 = require('md5')
+
+function convertEmailToUrl(strings, ...values) {
+  let newString = ""
+  strings.forEach((string, index) => {
+    newString += string
+    newString += values[index] ? md5(values[index]) : ""
+  })
+  return newString
+}
+
+function convertAvatar(email) {
+  const avatarUrl = convertEmailToUrl`https://www.gravatar.com/avatar/${email}?d=identicon`
+  return avatarUrl
+}
+
+module.exports = {
+  convertAvatar
+}
